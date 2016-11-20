b14c2d88be3661f94bfeb641e04ba81ee99175b7
Yahya Khan <yahyas.email@gmail.com>
2016-11-20 00:12:00 -0500

    Add files via upload

---
+++ yahya-final-project/platforms.js
@@ -0,0 +1,4 @@
+function Platforms(float w, float h, float previous) {
+  float 
+
+}
--- yahya-final-project/sketch.js
+++ yahya-final-project/sketch.js
@@ -1,10 +1,53 @@
+var speed = 1.5;
+var player;
+var initial = 200;
+var framecount = 0;
+
 function setup() {
-  createCanvas(700,500);
+  createCanvas(700, 500);
+  player = createVector(120, 350);
+  frameRate(60);
+  initial = width;
 }
-var speed = 1.1;
-scroll = createVector
+
 function draw() {
-  background(100,150,250);
-  createVector
-  
+
+  if (frameCount % 2 === 0) {
+    speed += .01;
+    console.log(speed);
+  }
+
+
+  //BLUE BACKGROUND
+  background(100, 150, 250);
+  fill(255);
+
+  //GROUND
+  noStroke();
+  fill(100, 220, 80);
+  rectMode(CORNERS);
+
+  if (initial >= 0) {
+    rect(0, player.y, initial -= speed, height);
+  }
+
+
+  //PLAYER
+  noStroke();
+  fill('MAGENTA');
+
+  rectMode(CORNER);
+  rect(player.x - 10, player.y - 20, 20, 20)
+
+  //VECTOR REFERENCE
+  stroke(100, 200, 10);
+  point(player.x, player.y);
+
+
+  //GROUND LINE
+  /*
+  stroke(255);
+  strokeWeight(1);
+  line(0, player.y, width, player.y);
+  */
 }
