de882724e9c835534009bb47c0eb473fef0d83d3
Yahya Khan <yahyas.email@gmail.com>
2016-11-22 00:06:13 -0500

    unexpected and uncaught errors galore

--- yahya-final-project/platforms.js
+++ yahya-final-project/platforms.js
@@ -1,4 +1,35 @@
-function Platforms(float w, float h, float previous) {
-  float 
+var Platform = function(previous, speed) {
+  this.lifespan = width;
+  this.velocity = speed;
+  this.position = previous + 30;
+  this.last = previous;
+  this.tall = random(300, 400);
 
+  this.run = function() {
+    this.update();
+    this.display();
+  };
+
+  this.update = function() {
+    this.position.add(speed);
+    this.lifespan -= speed;
+
+  }
+
+  this.display = function() {
+    strokeWeight(2);
+    stroke(60, 180, 50);
+    fill(100, 220, 80);
+    rectMode(CORNERS);
+    rect(width - position, this.tall, initial -= speed, height);
+
+  }
+  this.isDead = function() {
+    if (this.lifespan < 0.0) {
+      return true;
+
+    } else {
+      return false;
+    }
+  }
 }
--- yahya-final-project/sketch.js
+++ yahya-final-project/sketch.js
@@ -2,32 +2,31 @@ var speed = 1.5;
 var player;
 var initial = 200;
 var framecount = 0;
+var platforms = [];
 
 function setup() {
-  createCanvas(700, 500);
-  player = createVector(120, 350);
   frameRate(60);
+  createCanvas(700, 500);
+
+  player = createVector(120, 350);
   initial = width;
 }
 
 function draw() {
 
+  //BLUE BACKGROUND
+  background(100, 150, 250);
+  fill(255);
+
+  //INCREASES SPEED
   if (frameCount % 2 === 0) {
     speed += .01;
     console.log(speed);
   }
 
-  if (frameCount % 20 === 0) {
-    fill(255);
-    noStroke();
-    ellipse(20, 10, width + 20, random(30, 200));
-  }
-  //BLUE BACKGROUND
-  background(100, 150, 250);
-  fill(255);
-
-  //GROUND
-  noStroke();
+  //FIRST GROUND BLOCK
+  strokeWeight(2);
+  stroke(60, 180, 50);
   fill(100, 220, 80);
   rectMode(CORNERS);
 
@@ -35,17 +34,28 @@ function draw() {
     rect(0, player.y, initial -= speed, height);
   }
 
-
   //PLAYER
   noStroke();
   fill('MAGENTA');
-
   rectMode(CORNER);
   rect(player.x - 10, player.y - 20, 20, 20)
 
   //VECTOR REFERENCE
   stroke(100, 200, 10);
   point(player.x, player.y);
+
+  platforms.push(new Platform(width + 30, speed));
+
+  for (var i = platforms.length - 1; i >= 0; i--) {
+    console.log(platforms[i]);
+    var p = platforms[i];
+    p.run();
+    if (p.isDead()) {
+      //remove the particle
+      particles.splice(i, 1);
+    }
+  }
+
 
 
   //GROUND LINE
