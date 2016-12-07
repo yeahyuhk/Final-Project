df16a46d9af8072f38f5e0a03ab10cf4c5ff37c2
Yahya Khan <yahyas.email@gmail.com>
2016-12-07 00:47:57 -0500

    added comments explaining most of code + questions

--- yahya-final-project/sketch.js
+++ yahya-final-project/sketch.js
@@ -1,8 +1,8 @@
-var speed = 1.5;
+var speed = 1.5; // UNIVERSAL SPEED VARIABLE
 var player;
-var initial = 200;
+var initial;     // WIDTH VALUE FOR FIRST PLATFORM
 var framecount = 0;
-var platforms = [];
+var platforms = [];  // ARRAY FOR STORING NEWLY CREATED PLATFORMS
 
 function setup() {
   frameRate(60);
@@ -18,33 +18,39 @@ function draw() {
   background(70, 100, 210);
   fill(255);
 
-  speed += 0.005
-    //INCREASES SPEED
+  speed += 0.005   //CONSTANTLY INCREASES UNIVERSAL SPEED VARIABLE
 
-
-  //FIRST GROUND BLOCK
+  // APPEARANCE FOR PLATFORMS // GREEN
   strokeWeight(2);
   stroke(60, 180, 50);
   fill(100, 220, 80);
   rectMode(CORNERS);
 
-  if (initial >= 0) {
+  // DISPLAYS FIRST PLATFORM BLOCK UNTIL IT GOES OFF SCREEN (initial is width)
+  if (initial >= 0) {  
     rect(0, player.y, initial -= speed, height);
   }
-
 
   //VECTOR REFERENCE
   //stroke(100, 200, 10);
   //point(player.x, player.y);
 
-  if (frameCount % 60 === 0) {
-    platforms.push(new Platform(width + 30, speed));
-  }
 
+  // currently, I'm having trouble with this
+  // I want the platforms to show up faster over time using SPEED
+  // rather than be created a constant rate
+  
+  if (frameCount % 60 === 0) {  // NEW PLATFORM IS CREATED AT THIS RATE
+    platforms.push(new Platform(width + 30, speed));  // NEW PLATFORM IS ADDED TO PLATFORM ARRAY
+  }                                                   // WITH CURRENT SPEED
+                                                      // i want this speed to change over time
+ 
+  // taken from particle examples on github
   for (var i = platforms.length - 1; i >= 0; i--) {
-    //console.log(platforms[i]);
+  
     var p = platforms[i];
     p.run();
+    
     if (p.isDead()) {
       //remove the particle
       platforms.splice(i, 1);
