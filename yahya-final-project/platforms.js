98b12e59b812cd98dd2a4c64cb53301d2a1ce45b
Yahya Khan <yahyas.email@gmail.com>
2016-12-15 07:45:34 -0500

    Add files via upload

--- yahya-final-project/platforms.js
+++ yahya-final-project/platforms.js
@@ -4,7 +4,7 @@ function Platform(previous, initial) {
   this.platformStart = previous + speed * (difficulty * 2);
   this.platformHeight = random(250, 390);
   this.platformBottom = random(30, 100);
-  this.platformEnd = this.platformStart + random(210, 270) + (speed - 4) * 12 + difficulty * 20 + initial;
+  this.platformEnd = this.platformStart + random(210, 270) + speed* 15 + difficulty * 20 + initial;
 
   this.run = function() {
     this.update();
@@ -17,7 +17,7 @@ function Platform(previous, initial) {
 
   this.display = function() {
 
-    fill(255, this.platformEnd-20);
+    fill(255, this.platformEnd-50);
     stroke(255, this.platformEnd+20);
     strokeWeight(3);
     rectMode(CORNERS);
